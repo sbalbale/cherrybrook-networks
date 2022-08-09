ea14a2a048bff7fdd5f64818cd649431e8bf3370
Sean Balbale <sean.balbale@gmail.com>
2022-08-08 20:39:20 -0400

    fixed link

--- components/About.jsx
+++ components/About.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Link from "next/link";
 
 const About = () => {
   return (
-    <div id='about' className="w-full md:h-screen p-2 flex items-center py-16">
+    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
           <p className="uppercase text-xl tracking-widest text-[#f00043]">
@@ -10,14 +11,18 @@ const About = () => {
           </p>
           <h2 className="py-4">Who I Am</h2>
           <p className="py-2 text-gray-600">
-            I&apos;ve loved technology and engineering for as long as I can remember.
-            I taught myself how to code when I was ten and have been coding ever
-            since. Eight years later and I&apos;m now a senior in high school. I love
-            to code and create new things and always want to learn more!
+            I&apos;ve loved technology and engineering for as long as I can
+            remember. I taught myself how to code when I was ten and have been
+            coding ever since. Eight years later and I&apos;m now a senior in
+            high school. I love to code and create new things and always want to
+            learn more!
           </p>
-          <p className="py-2 text-gray-600 underline cursor-pointer">
-            Check out my latest projects
-          </p>
+
+          <Link href="/#projects">
+            <p className="py-2 text-gray-600 underline cursor-pointer">
+              Check out my latest projects
+            </p>
+          </Link>
         </div>
         <div></div>
       </div>
