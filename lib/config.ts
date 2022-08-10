27526e5b0331c15486a362ec5412d5628681c293
Sean Balbale <sean.balbale@gmail.com>
2022-08-10 16:42:54 -0400

    added notion

---
+++ lib/config.ts
@@ -0,0 +1,5 @@
+// TODO: change this to the notion ID of the page you want to test
+export const rootNotionPageId = 'f890057f749b484d855433bb83d57a89'
+
+export const isDev =
+  process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
