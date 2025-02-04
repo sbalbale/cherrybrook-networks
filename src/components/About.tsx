1f67e1d2c704d8704536ba2d446225ee015a21e6
Sean Balbale <sean.balbale@gmail.com>
2025-02-04 12:51:39 -0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,21 +9,24 @@ const About = () => {
           <h2 className="py-4 text-primary">About Me</h2>
           <p className="py-2 text-foreground">
             I&apos;m Sean Balbale, a sophomore and student-athlete at Trinity College
-            pursuing a double major in electrical and computer engineering and
-            computer science. Passionate about technology since age 10, I have
+            pursuing a double major in Electrical & Computer Engineering and
+            Computer Science. Passionate about technology since age 10, I have
             six years of experience in software engineering and technical
             support, assisting local businesses and students by designing custom
             computers, installing software and Wi-Fi systems, and automating
-            cloud backups. I have also worked in metrology, conducting
-            high-precision measurements during my internship at ECM, and
-            developed strong customer service skills as an apprentice optician
-            at Warby Parker. I thrive in fast-paced, collaborative environments,
-            balancing academics, athletics, and hands-on engineering projects.
-            Currently seeking a software engineering or embedded systems
-            internship, I am eager to apply my skills, tackle complex
-            challenges, and continue growing professionally. Let&apos;s connect!
-
-{/*             
+            cloud backups. My internship at East Coast Metrology allowed me to
+            conduct high-precision measurements and work with advanced metrology
+            equipment, while at Titan Advanced Energy Solutions, I developed
+            Python applications to visualize ultrasonic battery data, enhancing
+            defect detection processes for EV batteries. Additionally, I refined
+            my customer service and problem-solving skills as an apprentice
+            optician at Warby Parker. With a strong ability to balance
+            academics, athletics, and technical projects, I enjoy taking on
+            challenges that push me to think critically and collaborate
+            effectively. Currently seeking a software engineering or embedded
+            systems internship, I am eager to apply my skills, tackle complex
+            problems, and continue growing professionally. Let&apos;s connect!
+            {/*             
             I&apos;ve loved technology and engineering for as long as I can
             remember. I taught myself how to code when I was ten and have been
             coding ever since. Ten years later, I&apos;m a sophomore at Trinity
